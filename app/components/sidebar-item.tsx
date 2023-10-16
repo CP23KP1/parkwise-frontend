bba571f05388eacd3c95de1661f8469af1f45fe5
alonespace <jomcootle@gmail.com>
2023-10-16 17:12:40 +0700

    update: re style of side bar

--- app/components/sidebar-item.tsx
+++ app/components/sidebar-item.tsx
@@ -1,32 +1,36 @@
 "use client";
-import { usePathname } from 'next/navigation'
+import { usePathname } from "next/navigation";
 import Link from "next/link";
 
 export interface SidebarItemRenderProps {
-  name: string;
-  icon: string;
-  link: string;
-  open: boolean;
+    name: string;
+    icon: string;
+    link: string;
+    open: boolean;
 }
 
 const SidebarItem: React.FC<SidebarItemRenderProps> = ({
-  name,
-  icon,
-  link,
-  open,
+    name,
+    icon,
+    link,
+    open,
 }) => {
-  const router = usePathname()
-  const path = () => router === link
-  return (
-    <Link href={link}>
-    <div className={`grid grid-cols-3 w-full align-middle justify-center pt-3 hover:bg-slate-400 ${path() && 'bg-slate-400'} hover:cursor-pointer`}>
-      <div className="w-12 h-12 pl-4">
-        <img src={icon} />
-      </div>
-      <div className="mt-1">{open && <h1>{name}</h1>}</div>
-    </div>
-    </Link>
-  );
+    const router = usePathname();
+    const path = () => router === link;
+    return (
+        <Link href={link}>
+            <div
+                className={`transform hover:translate-x-1 grid grid-cols-3 w-auto align-middle justify-center my-2 mx-3 rounded-xl pt-3 hover:bg-slate-200 ${
+                    path() && "bg-slate-300"
+                } hover:cursor-pointer`}
+            >
+                <div className={`${open ? "pl-4 w-12 h-12" : "pl-2 w-8 h-8"}`}>
+                    <img src={icon} />
+                </div>
+                <div className="mt-1">{open && <h1>{name}</h1>}</div>
+            </div>
+        </Link>
+    );
 };
 
 export default SidebarItem;
--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -4,82 +4,88 @@ import { menuType } from "../assets/data/menu";
 import SidebarItem from "./sidebar-item";
 
 interface SidebarProps {
-  open?: boolean;
+    open?: boolean;
 }
 
 const Sidebar: React.FC<SidebarProps> = ({ open = false }) => {
-  const [openSidebar, setOpenSidebar] = useState(true);
-  const width = openSidebar ? "w-42 md:w-56" : "w-16";
-  const widthMobile = openSidebar ? "w-16 md:w-56" : "w-16";
+    const [openSidebar, setOpenSidebar] = useState(true);
+    const width = openSidebar ? "w-42 md:w-56" : "w-16";
+    const widthMobile = openSidebar ? "w-16 md:w-56" : "w-16";
 
-  const handleClickOpenSidebar = () => {
-    setOpenSidebar(!openSidebar);
-  };
+    const handleClickOpenSidebar = () => {
+        setOpenSidebar(!openSidebar);
+    };
 
-  const renderLogout = () => {
+    const renderLogout = () => {
+        return (
+            <div className="pt-4">
+                {openSidebar ? (
+                    <>
+                        <div className="cursor-pointer block text-center bg-red-500 text-white mx-6 p-3 rounded-md hover:bg-red-700">
+                            <button>Logout</button>
+                        </div>
+                    </>
+                ) : (
+                    <div className="w-12 h-12 pl-4 hover:cursor-pointer">
+                        <img src="/menu/logout.png" alt="Logout" />
+                    </div>
+                )}
+            </div>
+        );
+    };
+
     return (
-      <div className="pt-4">
-        {openSidebar ? (
-          <>
-            <div className="block text-center bg-red-500 text-white mx-6">
-              <button>Logout</button>
-            </div>
-          </>
-        ) : (
-          <div className="w-12 h-12 pl-4 hover:cursor-pointer">
-            <img src="/menu/logout.png" alt="Logout" />
-          </div>
-        )}
-      </div>
-    );
-  };
-
-  return (
-    <>
-      <div
-        className={`fixed h-screen transition-all duration-300 ${width} bg-zinc-100 rounded-r-3xl`}
-        style={{ top: 0, left: 0 }}
-      >
-        <div className="p-4 flex items-center justify-between">
-          <div className={`${openSidebar ? "w-14" : "pt-3"}`}>
+        <>
             <div
-              className="w-8 hover:bg-slate-500 hover:rounded hover:cursor-pointer"
-              onClick={handleClickOpenSidebar}
+                className={`fixed h-screen transition-all duration-300 ${width} bg-zinc-100 rounded-r-3xl`}
+                style={{ top: 0, left: 0 }}
             >
-              <img src="/menu/hamburger_menu.png" alt="Hamburger Menu" />
+                <div className="p-4 flex items-center justify-between">
+                    <div className={`${openSidebar ? "w-14" : "pt-3"}`}>
+                        <div
+                            className="w-8 hover:bg-slate-300 hover:rounded hover:cursor-pointer"
+                            onClick={handleClickOpenSidebar}
+                        >
+                            <img
+                                src="/menu/hamburger_menu.png"
+                                alt="Hamburger Menu"
+                            />
+                        </div>
+                    </div>
+                    {openSidebar && (
+                        <>
+                            <div className="w-10">
+                                <img
+                                    src="/logo/kmutt_logo.jpg"
+                                    alt="KMUTT Logo"
+                                />
+                            </div>
+                            <div>
+                                <h1>ParkWise</h1>
+                            </div>
+                        </>
+                    )}
+                </div>
+                <div className="w-full">
+                    {menuType.map((item) => {
+                        return (
+                            <SidebarItem
+                                key={item.name}
+                                icon={item.icon}
+                                name={item.name}
+                                link={item.link}
+                                open={openSidebar}
+                            />
+                        );
+                    })}
+                    {renderLogout()}
+                </div>
             </div>
-          </div>
-          {openSidebar && (
-            <>
-              <div className="w-14">
-                <img src="/logo/kmutt_logo.jpg" alt="KMUTT Logo" />
-              </div>
-              <div>
-                <h1>ParkWise</h1>
-              </div>
-            </>
-          )}
-        </div>
-        <div className="w-full">
-          {menuType.map((item) => {
-            return (
-              <SidebarItem
-                key={item.name}
-                icon={item.icon}
-                name={item.name}
-                link={item.link}
-                open={openSidebar}
-              />
-            );
-          })}
-          {renderLogout()}
-        </div>
-      </div>
-      <div
-        className={`h-screen transition-all duration-300 ${widthMobile} bg-zinc-100 rounded-r-3xl`}
-      ></div>
-    </>
-  );
+            <div
+                className={`h-screen transition-all duration-300 ${widthMobile} bg-zinc-100 rounded-r-3xl`}
+            ></div>
+        </>
+    );
 };
 
 export default Sidebar;
