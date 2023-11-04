fb6a68cd9c1393c34317accf9ec08c51062dfa7d
Thiraphat <few.thiraphat@gmail.com>
2023-11-04 23:08:56 +0700

    add more ui

--- app/dashboard/staff/page.tsx
+++ app/dashboard/staff/page.tsx
@@ -4,6 +4,8 @@ import ResponsiveStaffTable from "@/app/components/staff/staff-table";
 import React, { useState } from "react";
 import "react-responsive-modal/styles.css";
 import { Modal } from "react-responsive-modal";
+import FilterButton from "@/app/components/button/filter";
+import { FilterMenuProps } from "@/app/components/button/filter-menu";
 
 const Staff = () => {
   const data: StaffRowData[] = [
@@ -26,6 +28,21 @@ const Staff = () => {
       email: "jaroen@kmutt.ac.th",
       service: true,
       position: "อาจารย์",
+    },
+  ];
+
+  const filterData: FilterMenuProps[] = [
+    {
+      title: "ทั้งหมด",
+      func: () => console.log("ทั้งหมด"),
+    },
+    {
+      title: "ทั้งหมด",
+      func: () => console.log("ทั้งหมด"),
+    },
+    {
+      title: "ทั้งหมด",
+      func: () => console.log("ทั้งหมด"),
     },
   ];
   const [open, setOpen] = useState(false);
@@ -93,7 +110,17 @@ const Staff = () => {
         <div>
           <h1 className="text-xl font-bold">Staff</h1>
         </div>
-        <div className="flex justify-end">
+        <div className="flex justify-between my-4 align-middle">
+          <div className="w-10/12 flex align-middle">
+            <input
+              type="text"
+              className="border-2 border-solid border-gray-600 w-8/12 md:w-4/12 h-10 pl-3"
+              placeholder="ค้นหา"
+            />
+            <div className="mt-2 ml-2">
+              <FilterButton data={filterData as never} />
+            </div>
+          </div>
           <button
             className="btn bg-sky-400 py-2 px-4 rounded-md text-white"
             onClick={onOpenModal}
@@ -102,14 +129,18 @@ const Staff = () => {
           </button>
         </div>
         <ResponsiveStaffTable data={data} />
-        <style jsx>{`
-          .container {
-            margin: 50px;
-          }
-          p {
-            color: blue;
-          }
-        `}</style>
+
+         <div className="mt-8 flex align-middle gap-4">
+          <button className="flex items-center space-x-2  border-solid border-2 hover:bg-gray-200 text-white font-semibold py-2 px-4 rounded">
+            <img src="/svg/back-button.svg" className="w-5 h-5" />
+          </button>
+          <div>
+            <p className="text-center mt-2">1 / 14</p>
+          </div>
+          <button className="flex items-center space-x-2 border-solid border-2 hover:bg-gray-200 text-white font-semibold py-2 px-4 rounded">
+            <img src="/svg/next-button.svg" className="w-5 h-5" />
+          </button>
+        </div>
       </div>
     </>
   );
