37369a4cf6f8c5dd6c011732fea8adb55bef9a7c
alonespace <jomcootle@gmail.com>
2023-11-20 12:53:36 +0700

    add: rewrite

--- next.config.js
+++ next.config.js
@@ -3,17 +3,25 @@ const nextConfig = {
     images: {
         formats: ["image/webp"],
     },
-    basePath: process.env.NODE_ENV === "production" ? "/kp1" : "",
-    assetPrefix: process.env.NODE_ENV === "production" ? "/kp1/" : "",
-    redirects: async () => {
+    async rewrites() {
         return [
             {
-                source: "/kp1",
-                destination: "/kp1/login",
-                permanent: true,
+                source: "/kp1/:path*",
+                destination: "https://capstone23.sit.kmutt.ac.th/kp1/:path*",
             },
         ];
     },
+    // basePath: process.env.NODE_ENV === "production" ? "/kp1" : "",
+    // assetPrefix: process.env.NODE_ENV === "production" ? "/kp1/" : "",
+    // redirects: async () => {
+    //     return [
+    //         {
+    //             source: "/kp1",
+    //             destination: "/kp1/login",
+    //             permanent: true,
+    //         },
+    //     ];
+    // },
 };
 
 module.exports = nextConfig;
