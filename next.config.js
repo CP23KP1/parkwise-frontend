037a6b7757961a91b59d667233558b8442a89484
alonespace <jomcootle@gmail.com>
2023-11-20 13:31:52 +0700

    try: add base path

--- next.config.js
+++ next.config.js
@@ -11,6 +11,7 @@ const nextConfig = {
             },
         ];
     },
+    basePath: process.env.NODE_ENV === "production" ? "/kp1" : "",
     assetPrefix: process.env.NODE_ENV === "production" ? "/kp1/" : "",
 };
 
