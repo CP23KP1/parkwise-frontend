55a4f0603e0fd2ab15158154914029c7213dcaba
alonespace <jomcootle@gmail.com>
2023-11-20 11:46:01 +0700

    remove: rewrite

--- next.config.js
+++ next.config.js
@@ -4,14 +4,6 @@ const nextConfig = {
         formats: ["image/webp"],
     },
     basePath: "/kp1",
-    async rewrites() {
-        return [
-            {
-                source: "/:path*",
-                destination: "/kp1/:path*",
-            },
-        ];
-    },
 };
 
 module.exports = nextConfig;
