bca84655e4fbb74b70e39687a55d5ec7baa7d1cb
Thiraphat Itamonchai <42443713+fewthiraphat@users.noreply.github.com>
2023-11-23 14:11:42 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,14 +3,6 @@ const nextConfig = {
     images: {
         formats: ["image/webp"],
     },
-    async rewrites() {
-        return [
-            {
-                source: "/:path*",
-                destination: "/kp1/:path*",
-            },
-        ];
-    },
     assetPrefix: process.env.NODE_ENV === "production" ? "/kp1/" : "",
     publicRuntimeConfig: {
         basePath: process.env.NEXT_PUBLIC_BASEPATH,
